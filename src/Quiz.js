fcb5f086ec846f7409d201c3148a0ce8a2ad6c14
Ryan MacInnis <Ryan.MacInnis@lfg.com>
2019-12-30 17:10:42 -0500

    complete Module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,12 @@ class Quiz extends Component {
         }
     }
 
+    showNextQuestion() {
+        this.setState({
+            quiz_position: (this.state.quiz_position + 1),
+        });
+    }
+
     render() {
         const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length);
 
@@ -25,7 +31,8 @@ class Quiz extends Component {
             return (
                 <div>
                     <QuizQuestion 
-                        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+                        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                        showNextQuestionHandler={this.showNextQuestion.bind(this)} 
                     />
                 </div>
             )
